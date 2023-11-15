6eaaf36a0c98ccb2f7c15e1bb22ae1594ac6130e
AviweP <prudencedonovanmalopi@gmail.com>
2023-11-15 14:42:04 -0800

    my betty

--- my_betty.c
+++ my_betty.c
@@ -14,7 +14,7 @@ pid_t my_pid;  /**pid_ is a variable and the my_pid is a variable name **/
 
 my_pid = getpid();   /* the variable name get the process id*/
 
-while (my_pid);
+while (my_pid)
 {
 return (0);
 }
